d9f56a0cfd0af3f2e4f3d129e18cb93a7823a38f
Page Brooks <pagebrooks@gmail.com>
2012-03-04 11:23:20 -0800

    Adding initial unit test suite

---
+++ CombineTxt.Test/CombineTxtFixture.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace CombineTxt.Test
+{
+    [TestFixture]
+    public class CombineTxtFixture
+    {
+        [Test]
+        public void With_Creates_New_CombineTxtInfo_With_CombineTxt()
+        {
+            var c = CombineTxt.With("foo.txt");
+            Assert.AreEqual(typeof(CombineTxtInfoFixture), c.GetType());
+        }
+    }
+}
---
+++ CombineTxt.Test/CombineTxtInfoFixture.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace CombineTxt.Test
+{
+    [TestFixture]
+    public class CombineTxtInfoFixture
+    {
+        [Test]
+        public void DefineKeyBy_Sets_KeyDefinition()
+        {
+            var info = CombineTxt.With("foo.txt");
+            Func<string, string> func = s => string.Empty;
+            info.DefineKeyBy(func);
+            Assert.AreSame(func, info.KeyDefinition);
+        }
+
+        [Test]
+        public void DoNotDefineKey_Sets_Identity_Function_For_KeyDefinition()
+        {
+            var info = CombineTxt.With("foo.txt");
+            info.DoNotDefineKey();
+            Assert.AreEqual("foo", info.KeyDefinition("foo"));
+        }
+    }
+}
